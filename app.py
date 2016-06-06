3e1ddbe3a0dccef1274f7e032add5a7311eaf711
Reade <Reade@DN0a2230b5.SUNet>
2016-06-06 15:56:07 -0700

    update 4pm monday

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+import csv
 from flask import Flask 
+from helpers import get_data
 from flask import render_template
 app = Flask(__name__)
-
 
 @app.route("/")
 def homepage():
 	template = 'homepage.html'
+	object_list = get_data()
 	return render_template(template)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'results.html'
+    object_list = get_data()
+    for row in object_list:
+        if row['record_key'] == row_id:
+           return render_template(template, object=row)
+    abort(404)
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
--- helpers.py
+++ helpers.py
@@ -3,56 +3,64 @@ import requests
 import pandas as pd
 from os.path import join
 from operator import itemgetter
+from datetime import datetime
 
-DATA_FPATH_1 = './static/data/Death_in_Custody_2013.csv'
-DATA_FPATH_2 = './static/data/Data_Codes.csv'
-DATA_FPATH_3 = './static/data/location_codes.csv'
+custody_data_fname = './static/data/Death_in_Custody_2013.csv'
+offense_codes_fname = './static/data/Data_Codes.csv'
+location_codes_fname = './static/data/location_codes.csv'
 #DATA_FPATH_4 = geosomething 
 
+# BEST_FIELDS = ['reporting_agency', 'last_name', 'first_name', 
+# 	'middle_name', 'date_of_birth_mm', 'date_of_birth_dd', 
+# 	'date_of_birth_yyyy', 'race', 'custody_status', 'custody_offense',
+# 	'date_of_death_yyyy', 'date_of_death_mm', 'date_of_death_dd', 
+# 	'custodial_responsibility_at_time_of_death', 
+# 	'location_where_cause_of_death_occurred', 'facility_death_occured',
+# 	'manner_of_death', 'means_of_death', 'county', 'agency_number']
+
+BEST_FIELDS = ['full_name', 
+	'birthday', 'race', 'custody_status', 'custody_offense',
+	'date_of_death', 'custodial_responsibility_at_time_of_death', 
+	'location_where_cause_of_death_occurred',
+	'manner_of_death', 'means_of_death', 'county', 'record_key']
+
 def get_data():
-    datafile = join()
-    with open(datafile, 'r') as f:
-        c = csv.DictReader(f)
-        return list(c)
+    data = join()
+    data = add_variables(data)
+    data = trim(data)
+    return data
 
 def join():
-	deaths = pd.read_csv(DATA_FPATH_1)
-	codes = pd.read_csv(DATA_FPATH_2)
-	locations = pd.read_csv(DATA_FPATH_3)
+	deaths = pd.read_csv(custody_data_fname)
+	codes = pd.read_csv(offense_codes_fname)
+	locations = pd.read_csv(location_codes_fname, encoding="latin-1")
 	#names = pd.read_csv(DATA_FPATH_4)
-
-	add_continuous_variables()
-
-	dfa = deaths[['reporting_agency', 'last_name', 'first_name', 
-	'middle_name', 'date_of_birth_mm', 'date_of_birth_dd', 
-	'date_of_birth_yyyy', 'race', 'custody_status', 'custody_offense',
-	'date_of_death_yyyy', 'date_of_death_mm', 'date_of_death_dd', 
-	'custodial_responsibility_at_time_of_death', 
-	'location_where_cause_of_death_occurred', 'facility_death_occured',
-	'manner_of_death', 'means_of_death', 'county']]
-	dfb = codes[['name', 'custody_offense']]
-	dfc = locations[['agency_number', 'agency_name']]
-
-	temp = pd.merge(dfa, dfb, on='custody_offense')
-	dfx = pd.merge(temp, dfc, on='agency_number')
-	return dfx
-
-
-def add_continuous_variables():
+	temp = pd.merge(deaths, codes, on='custody_offense')
+	dfa = pd.merge(temp, locations, on='agency_number')
 	
+	return dfa
 
+def trim(data):
+	return data[BEST_FIELDS]
 
-	deaths['days'] = 
+def add_variables(data):
+	data['birthday_temp'] = data['date_of_birth_yyyy'].astype(str) + '-' + data['date_of_birth_mm'].astype(str) + '-' + data['date_of_birth_dd'].astype(str)
+	data['birthday'] = pd.to_datetime(data['birthday_temp'])
 
+	data['death_temp'] = data['date_of_death_yyyy'].astype(str) + '-' + data['date_of_death_mm'].astype(str) + '-' + data['date_of_death_dd'].astype(str)
+	data['date_of_death'] = pd.to_datetime(data['death_temp'])
 
-# MAKE TWO MORE VARIABLES, date of death and days in custody
+	data['age'] = ((data['date_of_death'] - data['birthday']).dt.days / 365).round()
 
+	data['full_name'] = data['last_name'].astype(str) + ', ' + data['first_name'].astype(str)
+
+	return data
 
 def sort_by_criteria(criteria, datarows):
-    if criteria == 'days_low':
-        rows = sorted(datarows, key=itemgetter('days'))
-    elif criteria == 'days_high':
-        rows = sorted(datarows, key=itemgetter('days'), reverse=True)
+    if criteria == 'youngest':
+        rows = sorted(datarows, key=itemgetter('age'))
+    elif criteria == 'oldest':
+        rows = sorted(datarows, key=itemgetter('age'), reverse=True)
     else:
        # i.e. 'date_recent' or any value...just sort by most recent
         rows = sorted(datarows, key=itemgetter('date_of_death'), reverse=True)   

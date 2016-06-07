cd07c84b233e0cd4ff350a71246095dc4dd1b4b7
Dan Nguyen <dansonguyen@gmail.com>
2016-06-06 23:33:17 -0700

    fun

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import csv
-from flask import Flask 
+from flask import Flask
 from helpers import get_data, sort_by_criteria
 from collections import Counter
 from flask import render_template, request
@@ -9,7 +9,7 @@ inmates = get_data()
 
 @app.route("/")
 def homepage():
-	
+
 	manner_of_death = Counter([d['manner_of_death'] for d in inmates])
 	manners_list = manner_of_death.most_common()
 	top_manners = manners_list[0:5]
@@ -22,11 +22,11 @@ def homepage():
 	locations_list = location_of_death.most_common()
 	top_locations = locations_list[0:5]
 
-	return render_template('homepage.html', 
+	return render_template('homepage.html',
 							top_manners=top_manners,
 							top_counties=top_counties,
 							top_locations=top_locations,
-							inmate_count = len(inmates), 
+							inmate_count = len(inmates),
 							top_inmates=inmates[0:20])
 
 @app.route('/results/')
@@ -39,7 +39,7 @@ def results():
 		if inmate_name.upper() in row['full_name'].upper():
 			filtered_inmates.append(row)
 	#filtered_data = sort_by_criteria(criteria=inmate_name, inmates=inmates)
-	
+
 	return render_template('results.html', inmates=filtered_inmates)
 
 @app.route('/county/<county_name>')
@@ -49,6 +49,12 @@ def county_page(county_name):
 		if county_name.upper() in row['county'].upper():
 			filtered_inmates.append(row)
 	return render_template('county.html', inmates=filtered_data)
+
+@app.route('/testchart')
+def test_chart():
+	stuff = Counter(r['manner_of_death'] for r in inmates)
+	return render_template('testchart.html', dataitems=stuff.most_common())
+
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
